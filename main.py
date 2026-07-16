fbed5e58dcda67b6c071f9d3037193a4d4c54517
unknown <beta98990@gmail.com>
2026-07-16 12:45:33 +0500

    loyiha

---
+++ main.py
@@ -0,0 +1,51 @@
+# mahsulotlar = ["Non", "Sut", "Yog'", "Guruch"]
+# narxlar = [3000, 8000, 25000, 12000]
+# print("Mahsulotlar va ularning narxlari:")
+# for i in range(len(mahsulotlar)):
+#     print(f"{mahsulotlar[i]} - {narxlar[i]} so'm")
+
+
+
+# son = list(range(1 , 31))
+# for son in range(1,31):
+#     if son % 3 == 0:
+#         print(f"{son} 3 ga bo'linadi")
+#     elif son % 5 == 0:
+#         print(f"{son} 5 ga bo'linadi")
+   
+
+
+
+
+# talabalar = ("Aziz", "Malika", "Jasur", "Nodira", "Bekzod")
+# ballar     = (85, 45, 92, 38, 67)
+# for i in range(len(talabalar)):
+#     if ballar[i] >= 60:
+#         print(f"{talabalar[i]} - {ballar[i]} ball, Siz imtihondan o'tdingiz.")
+#     else:
+#         print(f"{talabalar[i]} - {ballar[i]} ball, Siz imtihondan o'tmadingiz.")
+
+
+
+
+
+# sozlar = ["olma", "uzum", "behi", "shaftoli", "nok"]
+# uzun_soz = [0]
+# for soz in sozlar:
+#     if len(soz) > len(uzun_soz):
+#         uzun_soz = soz
+#         print(f"Eng uzun so'z: {uzun_soz} - {len(uzun_soz)} ta harf.")
+
+
+
+
+
+
+
+# ismlar = ("Aziz", "Vali", "Soli", "Guli")
+# masalalar_soni = (7, 9, 5, 10)  
+# for i in range(len(ismlar)):  
+#     if masalalar_soni[i] >= 8:
+#         print(f"Siz alo darajadasiz {ismlar[i]} - {masalalar_soni[i]} .")
+#     else:
+#         print(f"Qo'shimcha mashq kerak: {ismlar[i]} - {masalalar_soni[i]} .")
